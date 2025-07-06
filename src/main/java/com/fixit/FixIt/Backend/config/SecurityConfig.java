1b3f9c025b64dadad6e110c0c2fdf3e67db06299
Ayush301077 <luckyvsn0904@gmail.com>
2025-07-06 19:50:31 +0530

    updated Docker

--- src/main/java/com/fixit/FixIt/Backend/config/SecurityConfig.java
+++ src/main/java/com/fixit/FixIt/Backend/config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
         configuration.setAllowCredentials(true);
